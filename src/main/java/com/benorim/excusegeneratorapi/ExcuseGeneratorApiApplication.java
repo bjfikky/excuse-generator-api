6d015643483221646d78ebdd6ad6970c2c0fbce5
Benjamin Orimoloye <bjfikky@yahoo.com>
2022-10-09 00:47:05 -0400

    initial commit

---
+++ src/main/java/com/benorim/excusegeneratorapi/ExcuseGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.benorim.excusegeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcuseGeneratorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcuseGeneratorApiApplication.class, args);
+    }
+
+}
